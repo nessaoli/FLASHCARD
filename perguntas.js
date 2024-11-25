cd913024e80f0e7078f788b7185bf4be866d7115
Vanessa Oliveira <00001105879768sp@al.educacao.sp.gov.br>
2024-11-25 12:05:25 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -16,3 +16,20 @@ criaCartao(
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
+criaCartao (
+    'HTML',
+    'O que é uma tag <div>?',
+    'Um contêiner de bloco para agrupar elementos'
+)
+
+criaCartao (
+    'JavaScript',
+    'O que é JavaScript?',
+    'Linguagem para adicionar interatividade em páginas web'
+)
+
+criaCartao (
+    'CSS',
+    'O que é CSS?',
+    'É uma linguagem para estilizar páginas HTML'
+)
